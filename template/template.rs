b54ce6a9177e6c10e9f08546a8831cd9b20f57f4
hayaten0415 <hayaten415@gmail.com>
2023-06-12 21:31:25 +0900

    read macro

--- template/template.rs
+++ template/template.rs
@@ -1,19 +1,41 @@
-// https://img.atcoder.jp/intro-heuristics/editorial.pdf
-struct Input {
-    D: usize,
-    s: Vec<Vec<i64>>,
-    c: Vec<i64>
+ 
+macro_rules! read {
+	($($t:tt),*; $n:expr) => {{
+		let stdin = ::std::io::stdin();
+		let ret = ::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+			let line = line.unwrap();
+			let mut it = line.split_whitespace();
+			_read!(it; $($t),*)
+		}).collect::<Vec<_>>();
+		ret
+	}};
+	($($t:tt),*) => {{
+		let line = readln();
+		let mut it = line.split_whitespace();
+		_read!(it; $($t),*)
+	}};
 }
-
-fn compute_score(input: &Input, out: &Vec<usize>) -> i64 {
-    let mut score = 0;
-    let mut last = vec![0; 26];
-    for d in 0..out.len() {
-        last[out[d]] = d + 1;
-    for i in 0..26 {
-        score -= (d + 1 - last[i]) as i64 * input.c[i];
-    }
-        score += input.s[d][out[d]];
-    }
-    score
+ 
+macro_rules! _read {
+	($it:ident; [char]) => {
+		_read!($it; String).chars().collect::<Vec<_>>()
+	};
+	($it:ident; [u8]) => {
+		Vec::from(_read!($it; String).into_bytes())
+	};
+	($it:ident; usize1) => {
+		$it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1
+	};
+	($it:ident; [usize1]) => {
+		$it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>()
+	};
+	($it:ident; [$t:ty]) => {
+		$it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>()
+	};
+	($it:ident; $t:ty) => {
+		$it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e))
+	};
+	($it:ident; $($t:tt),+) => {
+		($(_read!($it; $t)),*)
+	};
 }
